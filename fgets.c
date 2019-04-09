d1124e203277758c8c5bed44c2bc18a4e33da403
mark-ma <uncle_drew81@126.com>
2019-04-09 10:33:00 +0800

    fgets file C-code test

---
+++ fgets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	FILE *stream;
+	char string[] = "This is a test";
+	char msg[20];
+
+	/*  open a file for update  */
+	stream = fopen("DUMMY.FIL", "w+");
+	/*  write a string into the file  */
+	fwrite(string, strlen(string), 1, stream);
+	/*  seek to the start of the file  */
+	fseek(stream, 0, SEEK_SET);
+	/*  read a string from the file  */
+	fgets(msg, strlen(string)+1, stream);
+	/*  display the string  */
+	printf("%s\n", msg);
+	fclose(stream);
+	return 0;
+}
